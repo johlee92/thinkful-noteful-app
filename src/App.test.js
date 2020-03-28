33b0e33f87a8a719b6ede92a56fda62f2244314f
JY <johnathanlee72@hotmail.com>
2020-03-28 11:15:55 -0600

    added a simple render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
+// make React available
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// make the ReactDOM available, necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the App component available
+import App from './App/App';
+import {BrowserRouter} from 'react-router-dom';
+
+// this is the test case
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
 });
