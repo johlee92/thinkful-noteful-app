d2f87515e4c85a0d4baf5401630468ea7a4bc57e
JY <johnathanlee72@hotmail.com>
2020-03-29 11:42:23 -0600

    updated client to link to deployed server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:9090',
+    // API_ENDPOINT: 'http://localhost:9090',
     // API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://limitless-chamber-91436.herokuapp.com',
 }
   
